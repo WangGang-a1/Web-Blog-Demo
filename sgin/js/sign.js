69e89ab9098efebd6b2d24b454c5eca7bf748687
nl-anonymous <59785609+WangGang-a1@users.noreply.github.com>
2022-11-17 10:44:23 +0800

    1

--- sgin/js/sign.js
+++ sgin/js/sign.js
@@ -62,13 +62,14 @@ window.onload=function(){
         var dataJson=window.localStorage.getItem('list');
         var success=null;
         var d =new Date();
-        var next=null;
+        var next = '<a id=1>'+'登录名: '+oUser.value+'</a>'+'<br>'+'<a id=2>'+'IP: '+returnCitySN["cip"]+'</a>'+'<br>'+'<a id=3>'+'IP所在地: '+
+                                returnCitySN["cname"]+'(仅供参考)'+'</a>'+'<br>'+'<a id=4>'+'登录时间详细: '+d+'</a>';
+
         dataJson=eval('(' + dataJson + ')');
         for (var i=0;i<dataJson.length;i++) {
             if (dataJson[i].username==login.username&&dataJson[i].password==login.password) {
                 success=1;
-                next = '<a id=1>'+'登录名: '+oUser.value+'</a>'+'<br>'+'<a id=2>'+'IP: '+returnCitySN["cip"]+'</a>'+'<br>'+'<a id=3>'+'IP所在地: '+
-                                returnCitySN["cname"]+'(仅供参考)'+'</a>'+'<br>'+'<a id=4>'+'登录时间详细: '+d+'</a>';
+                
                 document.write('<h1>恭喜登录成功</h1>'+next+
                                 '<br>'+'将在三秒内自动保存日志并跳转到管理页面'+
                                 "<script>"+
